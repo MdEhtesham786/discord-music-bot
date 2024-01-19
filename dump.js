ed878fcdd7b9e0b3eb6be06aa76aa26e398c0501
Hamza-Shaikh <hshs345678@gmail.com>
2024-01-20 00:22:27 +0530

    kuch bhi

--- dump.js
+++ dump.js
@@ -6,3 +6,8 @@
 // git remote add origin https://github.com/MdEhtesham786/discord-music-bot.git
 // git push -u origin main
 // nahi
+// first do git pull
+// git pull
+// git add .
+// git commit -m 'kucch bhi'
+// git push
